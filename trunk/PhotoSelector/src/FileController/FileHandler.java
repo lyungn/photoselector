134c6199119cea89037f715ad3b30244e7244748
lyungn@gmail.com <lyungn@gmail.com@8a555a64-00da-26e8-007f-74bf8ae6f32a>
2012-10-08 04:58:15 +0000

    nueva modificaciones, x cant F

--- trunk/PhotoSelector/src/FileController/FileHandler.java
+++ trunk/PhotoSelector/src/FileController/FileHandler.java
@@ -28,31 +28,30 @@ public class FileHandler {
         FileInputStream archivoSelected = null;
         FileOutputStream archivoSalida = null;
         try {
-            archivoSelected = new FileInputStream(orignFile);
-            archivoSalida = new FileOutputStream(destFile);
-            byte[] byteBuf = new byte[16384];
-            int numBytesRead;
-            while ((numBytesRead = archivoSelected.read(byteBuf)) != -1) {
-                archivoSalida.write(byteBuf, 0, numBytesRead);
+            if (fileRenamer(destFile, orignFile.getName())) {
+                archivoSelected = new FileInputStream(orignFile);
+                archivoSalida = new FileOutputStream(destFile);
+                byte[] byteBuf = new byte[16384];
+                int numBytesRead;
+                while ((numBytesRead = archivoSelected.read(byteBuf)) != -1) {
+                    archivoSalida.write(byteBuf, 0, numBytesRead);
+
+                }
+                archivoSalida.close();
+                archivoSelected.close();
+
+            } else {
+                
             }
+
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         } catch (IOException e) {
             System.out.println(e.getMessage());
-        } finally {
-            try {
-                archivoSalida.close();
-                archivoSelected.close();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                System.out.println(e.getMessage());
-            }
-
         }
     }
 
-    public static String fileSeacher(String stringFile,
-            String originFolderPath, String destFolderPath) {
+    public static String fileSeacher(String stringFile, String originFolderPath, String destFolderPath) {
 
         String returnString = "";
         File dirS = new File(originFolderPath);
@@ -62,12 +61,64 @@ public class FileHandler {
 
         } else {
             for (File f : files) {
-                ProgressBarFrame jPro = ProgressBarFrame.getInstance();
+                /*ProgressBarFrame jPro = ProgressBarFrame.getInstance();
                 jPro.setFileName(f.getName());
-                jPro.setActualValue(1);
+                jPro.setActualValue(1);*/
                 fileCopyer(f.getAbsolutePath(), destFolderPath, f.getName());
             }
         }
         return returnString;
+    }
+
+    private static boolean fileRenamer(File fileToRename, String oriFileName) {
+
+        String fileName = fileToRename.getName();
+        String tempName = null;
+        int pos = fileName.lastIndexOf(".");
+        if (pos > 0) {
+            tempName = fileName.substring(0, pos);
+        }
+        try {
+
+            String ss = fileToRename.getParent();
+            File filePath = new File(ss);
+            File[] files = filePath.listFiles(new MyFileFilter(tempName));
+            if (files.length == 0) {
+                return true;
+
+            } else {
+                if (files.length == 1) {
+                    for (File file : files) {
+                        String modifyFile = file.getName();
+                        int fileNameLen = tempName.length();
+
+                        modifyFile = modifyFile.substring(fileNameLen);
+                        if (modifyFile.contains("x") || modifyFile.contains("X")) {
+                            int posF = modifyFile.indexOf("F");
+                            String intRepeat = modifyFile.substring(1,posF);
+                            int cantToIncrease = Integer.parseInt(intRepeat);
+                            cantToIncrease = cantToIncrease +1;
+                            String newmodifle = ss + "\\" + tempName + "x" + String.valueOf(cantToIncrease) + "F" + ".jpg";
+                            File newName = new File(newmodifle);
+                            file.renameTo(newName);
+                            return false;
+                            
+                        } else {
+
+                            File newName = new File(filePath + "\\" + tempName + "x1F" + ".jpg");
+                            file.renameTo(newName);
+                            //fileToRename.delete();
+                            return false;
+                        }
+                    }
+                }
+                return false;
+
+
+            }
+        } catch (Exception e) {
+            System.out.println(e.getCause());
+        }
+        return false;
     }
 }
